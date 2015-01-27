dc4832d6bcfd7fb4d720786c868f54e475134888
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2015-01-28 00:01:17 +0100

    Ignore then remove forgotten .git directory in Flask archive

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -6,7 +6,12 @@ prep()
 
         # Disable "Makefile" to use "setup.py" instead
 	# (eups check first for existing "Makefile" and then for "setup.py")
-        FLASK_MAKEFILE=Makefile
+        FLASK_MAKEFILE="Makefile"
         [ -f ${FLASK_MAKEFILE} ] && mv ${FLASK_MAKEFILE} ${FLASK_MAKEFILE}.eups-removed
+
+        # Remove forgotten .git repository contained in Flask archive
+        # which might break eups
+        FORGOTTEN_GIT_DIR="docs/_themes/.git"
+        [ -e ${FORGOTTEN_GIT_DIR} ] && rm -r ${FORGOTTEN_GIT_DIR}
 }
 
